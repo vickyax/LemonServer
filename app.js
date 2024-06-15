e86b7d11d96cd7fcb18e51bec91b9b21324806b0
vickyax <vigneshr75021@gmail.com>
2024-06-15 15:34:08 +0530

    cors origin

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const cors = require('cors')
 const app = express();
 const mainRouter = require("./routes/user");
 
-
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
 app.use(express.json());
 
 app.use("/api/v1", mainRouter);
